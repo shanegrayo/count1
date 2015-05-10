87ccf859e29a91c63c78dc10f0af0db5ec19348e
unknown <shanegrayo@gmail.com>
2015-05-10 11:39:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user1/count/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.user1.count;mail
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private int count = 0;
+    private int[] images = new int[15];
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView;
+
+    }
+    public void pluss(View v) {
+        count = count + 1;
+        updatenumber();
+    }
+
+    private void updatenumber() {
+        // TODO Auto-generated method stub
+        textView.setText(String.valueOf(count));
+    }
+
+    public void minus(View v) {
+        count = count - 1;
+        updatenumber();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
